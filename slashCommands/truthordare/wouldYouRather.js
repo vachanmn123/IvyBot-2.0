7c7fb3c9067fc8bafc9525f051b0a2c40689d8cf
Vachan MN <vachanmn123@gmail.com>
2022-06-04 09:20:31 +0000

    would you rather

--- slashCommands/truthordare/wouldYouRather.js
+++ slashCommands/truthordare/wouldYouRather.js
@@ -4,22 +4,22 @@ const ee = require("../../botconfig/embed.json");
 const settings = require("../../botconfig/settings.json");
 const axios = require('axios');
 module.exports = {
-  name: "paranoia", //the command name for the Slash Command
-  description: "Gives a Paranoia question", //the command description for Slash Command Overview
+  name: "wouldyourather", //the command name for the Slash Command
+  description: "Gives a Would You Rather question", //the command description for Slash Command Overview
   cooldown: 1,
   memberpermissions: [], 
   requiredroles: [], 
   alloweduserids: [], 
   options: [
 		{"StringChoices": { name: "rating", description: "What rating do you want?", required: false, choices: [["pg", "pg"], ["pg13", "pg13"], ["r", "r"]] }}, 
-        {"User": { name: "user", description: "The reciever who will get the question", required: false }},
+        {"User": { name: "user", description: "The reciever who will get the quesion", required: false }},
   ],
   run: async (client, interaction) => {
     try{
-        const res = await axios.get(`https://api.truthordarebot.xyz/v1/paranoia?rating=${interaction.options.get("rating")? interaction.options.get("rating").value : "pg"}`);
+        const res = await axios.get(`https://api.truthordarebot.xyz/v1/wyr?rating=${interaction.options.get("rating")? interaction.options.get("rating").value : "pg"}`);
         const embed = new MessageEmbed()
-            .setColor("#ffff00")
-            .setTitle("Paranoia question")
+            .setColor("#ff00ff")
+            .setTitle("Would You Rather")
             .setDescription(res.data.question)
             .setTimestamp()
             .setAuthor({
