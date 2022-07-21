2181e8ac6080dfcc39ab4a73c24117fc272b5688
Vachan MN <vachanmn123@gmail.com>
2022-07-21 18:25:38 +0530

    Update together.js

--- slashCommands/games/together.js
+++ slashCommands/games/together.js
@@ -51,7 +51,7 @@ module.exports = {
             )
         interaction.reply({embeds: [emb]})
     } catch (e) {
-        interaction.reply(`An error occured \n \`\`\`e\`\`\``)
+        interaction.reply(`An error occured \n \`\`\`${e}\`\`\``)
         console.log(String(e.stack).bgRed)
     }
   }
